2cb353c1afa545a27dfc2a422d0ba832efbca2d2
Eduardo <eduardocorreass25@gmail.com>
2023-11-05 17:39:17 -0300

    feat: add deleteStudent js function

---
+++ script.js
@@ -0,0 +1,71 @@
+function addStudent() {
+    const name = document.getElementById("name").value
+    const age = document.getElementById("age").value
+    const address = document.getElementById("address").value
+    const email = document.getElementById("email").value
+
+    if (!validateName(name)) {
+        alert("Por favor, insira um nome válido.")
+        return
+    }
+
+    if (!validateAge(age)) {
+        alert("Por favor, insira uma idade válida entre 1 e 100.")
+        return
+    }
+
+    if (!validateAddress(address)) {
+        alert("Por favor, insira um endereço válido.")
+        return
+    }
+
+    if (!validateEmail(email)) {
+        alert("Por favor, insira um endereço de email válido.")
+        return
+    }
+
+    const student = {
+        name,
+        age,
+        address,
+        email
+    }
+
+    const students = JSON.parse(localStorage.getItem("students")) || []
+
+    students.push(student)
+
+    localStorage.setItem("students", JSON.stringify(students))
+
+    alert("Aluno cadastrado com sucesso!")
+}
+
+function deleteStudent(index) {
+    const students = JSON.parse(localStorage.getItem("students")) || []
+    if (index >= 0 && index < students.length) {
+        students.splice(index, 1)
+        localStorage.setItem("students", JSON.stringify(students))
+        alert("Aluno excluído com sucesso!")
+        listStudents()
+    } else {
+        alert("Índice inválido. Por favor, selecione um aluno válido para excluir.")
+    }
+}
+
+function validateName(name) {
+    return name.trim() !== ''
+}
+
+function validateAge(age) {
+    const ageNumber = parseInt(age, 10)
+    return !isNaN(ageNumber) && ageNumber >= 1 && ageNumber <= 100
+}
+
+function validateEmail(email) {
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return email.match(emailPattern)
+}
+
+function validateAddress(address) {
+    return address.trim() !== ''
+}
